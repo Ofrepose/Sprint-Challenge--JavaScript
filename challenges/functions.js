9cf1c81441f2fcf730ac8ac4153ef0ff837c7bc0
ofrepose <haroldfranklinpayne@gmail.com>
2020-02-21 22:03:16 -0500

    Completed Functions challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -5,7 +5,9 @@
   * The first two parameters can take any argument (we can pass any value as argument)
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+*/const consume = function(a,b,cb){
+  return cb(a,b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -14,11 +16,23 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (a,b) =>{
+  return a + b;
+}
+
+const multiply = (a,b) =>{
+  return a*b;
+}
+
+const greeting = (a,b)=>{
+  return `Hello ${a} ${b}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -29,6 +43,7 @@
 
 
 const external = "I'm outside the function";
+// nestedfunctions can access their parent variables. const internal is a variable declared in a function and can reach outward for value
 
 function myFunction() {
   console.log(external);
